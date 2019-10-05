6c42439428bf8c5be86e4beee10e192783a370ad
elharony <yahya@elharony.com>
2019-10-05 03:28:15 +0200

    Separate the JSON data from the 'App.js' file

---
+++ src/places.js
@@ -0,0 +1,87 @@
+export const places = [
+    {
+      "name": "Vevo Co-working Space",
+      "image": "vevo-co-working-space.jpg",
+      "address": "El-Mohandes El-Kordy, Maadi Al Khabiri Al Wasti, Al Maadi, Cairo, Egypt",
+      "phone": "0101 273 2732",
+      "lat": 29.9612892,
+      "lng": 31.2483158,
+      "facebook": "https://www.facebook.com/vevospace/",
+      "rating": 3,
+      "reviews": [
+        {
+          "review_user": "Vevo Last 1",
+          "review_text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, porro!",
+          "review_rate": 4
+        },
+        {
+          "review_user": "Vevo Last 2",
+          "review_text": "Ullam nesciunt iure dolores, nemo aperiam alias atque quos eum necessitatibus ex.",
+          "review_rate": 3
+        },
+        {
+          "review_user": "Vevo Last 3",
+          "review_text": "Earum temporibus amet dicta excepturi repellendus quo rerum doloremque, beatae illum optio? Vero cum saepe corrupti!",
+          "review_rate": 5
+        }
+      ]
+    },
+    {
+      "name": "Fab Lab Egypt",
+      "image": "fab-lab-egypt.jpg",
+      "address": "Villa 35 - 100 st. - Near Al Horia Square, Maadi, Maadi Al Khabiri Ash Sharqeyah, Cairo, Cairo Governorate",
+      "phone": "0101 746 5650",
+      "lat": 29.96175,
+      "lng": 31.2492591,
+      "facebook": "https://www.facebook.com/fablab.egypt/",
+      "rating": 4,
+      "reviews": [
+        {
+          "review_user": "Fab Lab Last 1",
+          "review_text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, porro!",
+          "review_rate": 4
+        },
+        {
+          "review_user": "Fab Lab Last 2",
+          "review_text": "Ullam nesciunt iure dolores, nemo aperiam alias atque quos eum necessitatibus ex.",
+          "review_rate": 3
+        },
+        {
+          "review_user": "Fab Lab Last 3",
+          "review_text": "Earum temporibus amet dicta excepturi repellendus quo rerum doloremque, beatae illum optio? Vero cum saepe corrupti!",
+          "review_rate": 5
+        }
+      ]
+    },
+    {
+      "name": "Makanak Office Space",
+      "image": "makanak-office-space.jpg",
+      "address": "23 El-Nahda Street, 7 St, Maadi Sarayat, El Maleka Tower, Second Floor. Maadi, Maadi Al Khabiri Ash Sharqeyah, Cairo, Cairo Governorate 11431",
+      "phone": "0120 000 2092",
+      "lat": 29.9601,
+      "lng": 31.2532501,
+      "facebook": "https://www.facebook.com/makanakoffice/",
+      "rating": 3,
+      "reviews": [
+        {
+          "review_user": "Makanak Last 1",
+          "review_text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, porro!",
+          "review_rate": 4
+        },
+        {
+          "review_user": "Makanak Last 2",
+          "review_text": "Ullam nesciunt iure dolores, nemo aperiam alias atque quos eum necessitatibus ex.",
+          "review_rate": 3
+        },
+        {
+          "review_user": "Makanak Last 3",
+          "review_text": "Earum temporibus amet dicta excepturi repellendus quo rerum doloremque, beatae illum optio? Vero cum saepe corrupti!",
+          "review_rate": 5
+        }
+      ]
+    }
+]
+
+export default {
+    places,
+}
